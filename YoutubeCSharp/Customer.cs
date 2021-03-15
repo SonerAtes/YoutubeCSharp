e200eae9df0b73f9bdb6e5792fc1b08359435f8e
Soner ATEŞ <captain_REX_camino@hotmail.com>
2021-03-16 01:40:51 +0300

    Add project files.

---
+++ YoutubeCSharp/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoutubeCSharp
+{
+    class Customer
+    {
+        //Field
+        //public string FirstName;
+
+        //Property
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+
+        //private string _firstName;
+        //public string FirstName
+        //{
+        //    get { return "Mr." + _firstName; }
+        //    set { _firstName = value; }
+        //}
+    }
+}
---
+++ YoutubeCSharp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace YoutubeCSharp
+{
+    //CLASS lar içerilerinde metotları barındırır. Yani içerisindeki methotları gruplandırır.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Uptade();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Soner";
+            customer.LastName = "ATEŞ";
+            customer.City = "ANTALYA";
+            Console.WriteLine(customer.FirstName + " " + customer.LastName);
+
+            //**Alttaki ile Üstteki arasında hiçbir fark yok iki şekilde de kullanılabilir.
+
+            Customer customer2 = new Customer
+            {
+                Id = 2, FirstName="İlknur", LastName="ATEŞ", City="BURDUR"
+            };
+            Console.WriteLine(customer2.FirstName + " " + customer2.LastName);
+
+            Console.ReadLine();
+        }
+    }
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added !");
+        }
+
+        public void Uptade()
+        {
+            Console.WriteLine("Customer Updated !");
+        }
+    }
+
+    class ProductManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Product Added !");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Product Updated !");
+        }
+    }
+}
