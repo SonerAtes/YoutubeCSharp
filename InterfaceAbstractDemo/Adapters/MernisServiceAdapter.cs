c83bd1ba5c0b2a2eeed41af33712a691dc839db3
Soner ATEŞ <captain_REX_camino@hotmail.com>
2021-03-30 03:54:12 +0300

    Starbucks and Nero

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,17 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Adepters
+{
+    class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient();
+            return client.TCKimlikDogrula(Convert.ToInt64(customer.NationalityId),customer.FirstName.ToUpper(), customer.LastName.ToUpper(), customer.DateOfBirth.Year);
+        }
+    }
+}
